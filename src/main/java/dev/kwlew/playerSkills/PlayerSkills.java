79edda15fd4d79809466ff75fbe1da6b6bbfe068
Kaleo Soares <kaleosoaresmendonca@gmail.com>
2026-06-03 07:21:55 -0300

    Initial commit.

---
+++ src/main/java/dev/kwlew/playerSkills/PlayerSkills.java
@@ -0,0 +1,16 @@
+package dev.kwlew.playerSkills;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerSkills extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // b
+    }
+
+    @Override
+    public void onDisable() {
+        // a
+    }
+}
